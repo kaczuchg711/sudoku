3000c3fa7968435925a2fab5353867572415a6be
Tomek <kaczatom4@gmail.com>
2020-06-30 19:56:23 +0200

    add Lisners

--- src/main/java/pl/c0/kaczuch/sudoku/controller/SudokuController.java
+++ src/main/java/pl/c0/kaczuch/sudoku/controller/SudokuController.java
@@ -2,20 +2,38 @@ package pl.c0.kaczuch.sudoku.controller;
 
 import pl.c0.kaczuch.sudoku.model.SudokuModel;
 import pl.c0.kaczuch.sudoku.view.SudokuView;
+import sun.jvm.hotspot.HelloWorld;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.Document;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SudokuController
 {
     SudokuModel model;
     SudokuView view;
 
-    public SudokuController(SudokuModel model,SudokuView view)
+    public SudokuController(SudokuModel model, SudokuView view)
     {
         this.model = model;
         this.view = view;
         this.view.addStatementListener(new GenListener());
+
+        CheckListener[][] checkListeners = new CheckListener[9][9];
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                checkListeners[i][j] = new CheckListener();
+            }
+        }
+
+        this.view.addNumberInListener(checkListeners);
     }
 
     private class GenListener implements ActionListener
@@ -26,6 +44,39 @@ public class SudokuController
             int[][] numbers;
             numbers = model.get_numbers_from_file();
             view.genNumbers(numbers);
+        }
+    }
+
+    private class CheckListener implements DocumentListener, FocusListener
+    {
+        @Override
+        public void insertUpdate(DocumentEvent e)
+        {
+            System.out.println("public void insertUpdate(DocumentEvent e)" + e.hashCode());
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e)
+        {
+            System.out.println("public void removeUpdate(DocumentEvent e)");
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e)
+        {
+            System.out.println("public void changedUpdate(DocumentEvent e)");
+        }
+
+        @Override
+        public void focusGained(FocusEvent e)
+        {
+            System.out.println("     public void focusGained(FocusEvent e)");
+        }
+
+        @Override
+        public void focusLost(FocusEvent e)
+        {
+            System.out.println("        public void focusLost(FocusEvent e)");
         }
     }
 
--- src/main/java/pl/c0/kaczuch/sudoku/view/SudokuView.java
+++ src/main/java/pl/c0/kaczuch/sudoku/view/SudokuView.java
@@ -1,8 +1,13 @@
 package pl.c0.kaczuch.sudoku.view;
 
 import javax.swing.*;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.Document;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusListener;
+import java.util.ArrayList;
+import java.util.EventListener;
 
 public class SudokuView extends JFrame
 {
@@ -11,7 +16,6 @@ public class SudokuView extends JFrame
 
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
-
 
     public SudokuView()
     {
@@ -36,22 +40,39 @@ public class SudokuView extends JFrame
         this.getButtonPanel().getButton("gen").addActionListener(listenForGen);
     }
 
+    public void addNumberInListener(EventListener[][] lisnerForNF)
+    {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                for (int k = 0; k < 3; k++)
+                    for (int l = 0; l < 3; l++)
+                    {
+                        this.boardPanel.getNumberBoxes()[i][j].getNf()[k][l].getDocument().addDocumentListener((DocumentListener) lisnerForNF[j + i * 3][l + k * 3]);
+                        this.boardPanel.getNumberBoxes()[i][j].getNf()[k][l].addFocusListener((FocusListener) lisnerForNF[j + i * 3][l + k * 3]);
+                    }
+    }
+
     public ButtonPanel getButtonPanel()
     {
         return buttonPanel;
     }
 
+    public BoardPanel getBoardPanel()
+    {
+        return boardPanel;
+    }
+
     public void genNumbers(int[][] numbers)
     {
-
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
                 for (int ii = 0; ii < 3; ii++)
                     for (int jj = 0; jj < 3; jj++)
                     {
-                        if(numbers[ii + i * 3][jj + j * 3] == 0)
+                        if (numbers[ii + i * 3][jj + j * 3] == 0)
                             continue;
                         this.boardPanel.getNumberBoxes()[i][j].getNf()[ii][jj].setNumber(numbers[ii + i * 3][jj + j * 3]);
                     }
     }
+
 }
